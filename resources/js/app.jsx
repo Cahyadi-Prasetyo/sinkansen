1781a5771ad8691b53dc3a7df4cd0803a896e532
Cahyadi-Prasetyo <cahyadiprasetyo659@gmail.com>
2026-09-17 15:02:53 +0700

    feat(fe): setup React 19 with Vite, Tailwind CSS, and entry component

---
+++ resources/js/app.jsx
@@ -0,0 +1,14 @@
+import './bootstrap';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import MainApp from './MainApp';
+
+const container = document.getElementById('root');
+if (container) {
+    const root = createRoot(container);
+    root.render(
+        <React.StrictMode>
+            <MainApp />
+        </React.StrictMode>
+    );
+}
